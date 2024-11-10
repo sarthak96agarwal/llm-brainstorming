b66c51c5f538bf238dc4372ac2effaccada5ef58
Sarthak Agarwal <sarthak96agarwal@gmail.com>
2024-11-10 17:07:52 -0500

    Add story search app

---
+++ story_search/search.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import os
+from langchain.llms import OpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Setup LLM
+llm = OpenAI(temperature=0)
+
+prompt=ChatPromptTemplate.from_template(
+"""
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
+
+"""
+)
+
+def vector_embedding():
+    if "vectors" not in st.session_state:
+
+        st.session_state.embeddings=OpenAIEmbeddings()
+        st.session_state.loader=PyPDFDirectoryLoader("./stories") ## Data Ingestion
+        st.session_state.docs=st.session_state.loader.load() ## Document Loading
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) #splitting
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings) #vector OpenAI embeddings
+
+vector_embedding()
+
+st.title("Query a Story Demo")
+
+# Sidebar for PDF links
+st.sidebar.title("Available Stories")
+pdf_link = "https://arxiv.org/pdf/1706.03762"
+st.sidebar.markdown(f"[Attention is all you need]({pdf_link})", unsafe_allow_html=True)
+
+prompt1=st.text_input("Enter Your Question From Story")
+
+if prompt1:
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    retriever=st.session_state.vectors.as_retriever()
+    retrieval_chain=create_retrieval_chain(retriever,document_chain)
+    response=retrieval_chain.invoke({'input':prompt1})
+    st.write(response['answer'])
+
+    # With a streamlit expander
+    with st.expander("Story Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
+
+
+
+
+
